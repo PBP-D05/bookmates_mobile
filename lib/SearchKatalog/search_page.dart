aa6d987ea6621d80701dc83c8aeb1125f57eaec5
joydebora <joy.debora@ui.ac.id>
2023-12-11 13:22:22 +0700

    fixing routing and UI SearchKatalog

--- lib/SearchKatalog/search_page.dart
+++ lib/SearchKatalog/search_page.dart
@@ -1,7 +1,6 @@
-// File: lib/screens/search_katalog/search_page.dart
 import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:bookmates_mobile/models/buku.dart';
 import 'show_book.dart';
 
@@ -32,7 +31,7 @@ class _SearchPageState extends State<SearchPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Search Catalog'),
-        backgroundColor: Color.fromARGB(193, 255, 88, 88),
+        backgroundColor: Colors.pink, // Warna background page
         foregroundColor: Colors.white,
       ),
       body: SingleChildScrollView(
@@ -48,27 +47,29 @@ class _SearchPageState extends State<SearchPage> {
                   style: TextStyle(
                     fontSize: 30,
                     fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(193, 255, 88, 88),
+                    color: Colors.pink, // Warna tulisan judul
                   ),
                 ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
+                  const SizedBox(width: 20),
                   Text(
                     'Select Category:',
                     style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(193, 255, 88, 88),
+                      color: Colors.pink, // Warna tulisan Select Category
                     ),
                   ),
+                  const SizedBox(width: 10),
                   Container(
                     padding: EdgeInsets.symmetric(horizontal: 10),
                     decoration: BoxDecoration(
-                      border: Border.all(color: Color.fromARGB(193, 0, 0, 0)),
+                      border: Border.all(color: Colors.black),
                       borderRadius: BorderRadius.circular(20),
-                      color: Color.fromARGB(193, 255, 88, 88),
+                      color: Colors.pink, // Warna dropdown background
                     ),
                     child: DropdownButton<String>(
                       value: _selectedCategory,
@@ -81,14 +82,15 @@ class _SearchPageState extends State<SearchPage> {
                           .map<DropdownMenuItem<String>>((String value) {
                         return DropdownMenuItem<String>(
                           value: value,
-                          child: Text(value),
+                          child: Text(value, style: TextStyle(color: Colors.black)), // Warna dropdown item
                         );
                       }).toList(),
-                      style: TextStyle(color: Color.fromARGB(193, 0, 0, 0)),
-                      icon: Icon(Icons.arrow_drop_down, color: Color.fromARGB(193, 255, 88, 88)),
+                      style: TextStyle(color: Colors.black),
+                      icon: Icon(Icons.arrow_drop_down, color: Colors.white),
                       underline: Container(),
                     ),
                   ),
+                  const SizedBox(width: 10),
                   Expanded(
                     child: Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -103,12 +105,13 @@ class _SearchPageState extends State<SearchPage> {
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20.0),
                           ),
-                          fillColor: Color.fromARGB(193, 255, 88, 88),
+                          fillColor: Colors.white, // Warna background text field
                           filled: true,
                         ),
                       ),
                     ),
                   ),
+                  const SizedBox(width: 10),
                   ElevatedButton(
                     onPressed: () {
                       performSearch();
@@ -121,12 +124,13 @@ class _SearchPageState extends State<SearchPage> {
                       ),
                     ),
                     style: ElevatedButton.styleFrom(
-                      primary: Color.fromARGB(193, 255, 88, 88),
+                      primary: Colors.pink, // Warna tombol Search
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20.0),
                       ),
                     ),
                   ),
+                  const SizedBox(width: 20),
                 ],
               ),
               const SizedBox(height: 20),
@@ -138,39 +142,84 @@ class _SearchPageState extends State<SearchPage> {
     );
   }
 
-
   Widget _buildSearchResults() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Text(
-          'Search Results:',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Color.fromARGB(193, 255, 88, 88),
-          ),
-        ),
         const SizedBox(height: 10),
-        Column(
-          children: _searchResults.map((buku) {
-            // Membersihkan deskripsi sebelum ditampilkan
+        GridView.builder(
+          shrinkWrap: true,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10.0,
+            mainAxisSpacing: 10.0,
+            childAspectRatio: 3.3,
+          ),
+          itemCount: _searchResults.length,
+          itemBuilder: (context, index) {
+            Buku buku = _searchResults[index];
             String cleanedDescription = cleanDescription(buku.fields.desc);
-            return ListTile(
-              title: Text(buku.fields.judul),
-              subtitle: Text('Author: ${buku.fields.author}, Age: ${buku.fields.minAge}-${buku.fields.maxAge}'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ShowBook(buku: buku, cleanedDescription: cleanedDescription),
-                  ),
-                );
-              },
-            );
-          }).toList(),
+
+            return _buildBookCard(buku, cleanedDescription);
+          },
         ),
       ],
+    );
+  }
+
+  Widget _buildBookCard(Buku buku, String cleanedDescription) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ShowBook(buku: buku, cleanedDescription: cleanedDescription),
+            ),
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 120,
+                height: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  image: DecorationImage(
+                    image: NetworkImage(buku.fields.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      buku.fields.judul,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text('By ${buku.fields.author}'),
+                    Text('Age ${buku.fields.minAge}-${buku.fields.maxAge}'),
+                    SizedBox(height: 3),
+                    generateRatingStars(buku.fields.rating),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
@@ -183,24 +232,43 @@ class _SearchPageState extends State<SearchPage> {
       },
     );
 
-  try {
-    if (response.statusCode == 200) {
-      // Successful response, parse the data
-      List<dynamic> responseData = jsonDecode(response.body);
-      setState(() {
-        _searchResults = (responseData as List).map((json) => Buku.fromJson(json)).toList();
-        // _searchResults = Buku.fromJson(responseData);
-      });
-      print(responseData);
-        
-    } else {
-      // Handle error cases
-      print("Error: ${response.statusCode}");
+    try {
+      if (response.statusCode == 200) {
+        List<dynamic> responseData = jsonDecode(response.body);
+        setState(() {
+          _searchResults = (responseData as List).map((json) => Buku.fromJson(json)).toList();
+        });
+        print(responseData);
+      } else {
+        print("Error: ${response.statusCode}");
+      }
+    } catch (error) {
+      print("Exception: $error");
     }
-  } catch (error) {
-
-    // Handle exceptions
-    print("Exception: $error");
   }
-}
+
+  Widget generateRatingStars(double rating) {
+    int roundedRating = rating.floor();
+    List<Widget> stars = [];
+
+    for (int i = 0; i < roundedRating; i++) {
+      stars.add(Icon(
+        Icons.star,
+        color: Colors.yellow,
+        size: 25,
+      ));
+    }
+
+    for (int j = roundedRating; j < 5; j++) {
+      stars.add(Icon(
+        Icons.star_border,
+        color: Colors.yellow,
+        size: 25,
+      ));
+    }
+
+    return Row(
+      children: stars,
+    );
+  }
 }
