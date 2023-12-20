19d7b9192d9d9b9dd0da497a835f4d18e331fe41
Yumeausealot <dfrfathur18@gmail.com>
2023-12-20 21:06:57 +0700

    leaderboard overflow fix

--- lib/ReviewerLeaderboard/screen/leaderboard.dart
+++ lib/ReviewerLeaderboard/screen/leaderboard.dart
@@ -15,20 +15,16 @@ class LeaderboardPage extends StatefulWidget {
 }
 
 class _LeaderboardPageState extends State<LeaderboardPage> {
-
   @override
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
     Future<List<Reviewer>> data = request
-    .get('https://booksmate-d05-tk.pbp.cs.ui.ac.id/challenge/ranking/')
-    .then((value) {
+        .get('https://booksmate-d05-tk.pbp.cs.ui.ac.id/challenge/ranking/')
+        .then((value) {
       if (value == null) {
         return [];
       }
-      // print(value);
       var jsonValue = jsonDecode(value);
-      // print("JS VAL $jsonValue");
-      // print("JS VAL ${jsonValue['text']}");
       List<Reviewer> listItem = [];
       for (var data in jsonValue) {
         if (data != null) {
@@ -46,97 +42,104 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
       ),
       drawer: const LeftDrawer(),
       body: FutureBuilder(
-        future: data,
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.data == null) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            if (!snapshot.hasData) {
-              return const Column(
-                children: [
-                  Text(
-                    "Tidak ada data reviewer.",
-                    style:
-                      TextStyle(color: Color(0xff59A5D8), fontSize: 20),
-                  ),
-                  SizedBox(height: 8),
-                ],
-              );
+          future: data,
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return const Center(child: CircularProgressIndicator());
             } else {
+              if (!snapshot.hasData) {
+                return const Column(
+                  children: [
+                    Text(
+                      "Tidak ada data reviewer.",
+                      style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                    ),
+                    SizedBox(height: 8),
+                  ],
+                );
+              } else {
+                List<Reviewer> sortedList = List.from(snapshot.data)
+                  ..sort((a, b) =>
+                      b.fields.banyakReview.compareTo(a.fields.banyakReview));
 
-              // Sort listview based on the total review
-              List<Reviewer> sortedList = List.from(snapshot.data)
-                ..sort((a, b) => b.fields.banyakReview.compareTo(a.fields.banyakReview));
+                double screenWidth = MediaQuery.of(context).size.width;
 
-              return ListView.builder(
-                itemCount: sortedList.length,
-                itemBuilder: (_, index) => Container(
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 16, vertical: 12),
-                  padding: const EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 2,
-                        blurRadius: 5,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
+                return ListView.builder(
+                  itemCount: sortedList.length,
+                  itemBuilder: (_, index) => Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 12),
+                    padding: const EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "${index + 1}.",
+                          style: TextStyle(
+                            fontSize: screenWidth * 0.03,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(width: 16),
+                        Expanded(
+                          child: Text(
+                            "Username: ${sortedList[index].fields.user}",
+                            style: TextStyle(
+                              fontSize: screenWidth * 0.02,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 16),
+                        Expanded(
+                          child: Text(
+                            "Is a Writer: ${sortedList[index].fields.isGuru}",
+                            style: TextStyle(
+                              fontSize: screenWidth * 0.02,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 16),
+                        Expanded(
+                          child: Text(
+                            "Total Review: ${sortedList[index].fields.banyakReview}",
+                            style: TextStyle(
+                              fontSize: screenWidth * 0.02,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 16),
+                        Expanded(
+                          child: Text(
+                            "Total Stars: ${sortedList[index].fields.banyakBintang}",
+                            style: TextStyle(
+                              fontSize: screenWidth * 0.02,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        "${index + 1}.",
-                        style: const TextStyle(
-                          fontSize: 25.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(width: 150),
-                      Text(
-                        "Username: ${sortedList[index].fields.user}",
-                        style: const TextStyle(
-                          fontSize: 17.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(width: 100),
-                      Text(
-                        "Is a Writer: ${sortedList[index].fields.isGuru}",
-                        style: const TextStyle(
-                          fontSize: 17.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(width: 100),
-                      Text(
-                        "Total Review: ${sortedList[index].fields.banyakReview}",
-                        style: const TextStyle(
-                          fontSize: 17.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(width: 100),
-                      Text(
-                        "Total Stars: ${sortedList[index].fields.banyakBintang}",
-                        style: const TextStyle(
-                          fontSize: 17.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              );
+                );
+              }
             }
-          }
-        }
-      )
+          }),
     );
   }
 }
